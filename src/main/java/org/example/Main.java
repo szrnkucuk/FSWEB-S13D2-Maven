c848b14f8f525600b656756c84b0d310cac55b5c
szrnkucuk <153090583+szrnkucuk@users.noreply.github.com>
2025-04-24 12:51:11 +0300

    mission  completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,57 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(12212));
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        String numberStr = Integer.toString(number);
+        String reversedNumber = new StringBuilder(numberStr).reverse().toString();
+        return numberStr.equals(reversedNumber);
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+
+    public static String numberToWords(int number) {
+        if (number < 0) {
+            return "Invalid Value";
+        }
+
+        String[] digitWords = {
+                "Zero", "One", "Two", "Three", "Four",
+                "Five", "Six", "Seven", "Eight", "Nine"
+        };
+
+        String numberStr = String.valueOf(number);
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numberStr.length(); i++) {
+            char digitChar = numberStr.charAt(i);
+            int digit = digitChar - '0';
+            result.append(digitWords[digit]).append(" ");
+        }
+
+        return result.toString().trim();
+
     }
 }
